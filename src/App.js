cc582a995c41a6d9a3067179745661bbeffb0a6c
Arka Bhuiyan <os-arka.bhuiyan@rakuten.com>
2023-04-18 13:21:07 +0600

    Adding all files initially

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          Your Todo's List
+        </div>
+        <div>
+          <table border='1'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Learn React</td>
+                <td>Arka</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Learn Django</td>
+                <td>Arka</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
