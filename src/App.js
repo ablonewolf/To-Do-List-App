8d25d566789723ea817d0909263c9111f817b126
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-04-21 11:15:20 +0600

    added a component for table and loaded the toDo list dynamically using the map function.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import './App.css';
-import ToDoRowItem from './components/ToDoRowItem';
-
+import TodoTable from './components/TodoTable';
+const toDos = [
+  { rowNumber: 1, taskName: 'Learn React', taskAssignee: 'Arka' },
+  { rowNumber: 2, taskName: 'Revise Python', taskAssignee: 'Arka' },
+  { rowNumber: 3, taskName: 'Learn Flask', taskAssignee: 'Arka' },
+  { rowNumber: 4, taskName: 'Learn Django', taskAssignee: 'Arka' }
+];
 function App() {
-  const toDos = [
-    { rowNumber: 1, taskName: 'Learn React', taskAssignee: 'Arka' },
-    { rowNumber: 2, taskName: 'Revise Python', taskAssignee: 'Arka' },
-    { rowNumber: 3, taskName: 'Learn Flask', taskAssignee: 'Arka' },
-    { rowNumber: 4, taskName: 'Learn Django', taskAssignee: 'Arka' }
-  ];
-
   return (
     <div className='mt-5 container'>
       <div className='card'>
         <div className='card-header'>Your Todo's List</div>
         <div className='card-body'>
-          <table className='table table-hover'>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Description</th>
-                <th scope='col'>Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <ToDoRowItem
-                rowNumber={toDos[0].rowNumber}
-                taskName={toDos[0].taskName}
-                taskAssignee={toDos[0].taskAssignee}
-              />
-              <ToDoRowItem
-                rowNumber={toDos[1].rowNumber}
-                taskName={toDos[1].taskName}
-                taskAssignee={toDos[1].taskAssignee}
-              />
-              <ToDoRowItem
-                rowNumber={toDos[2].rowNumber}
-                taskName={toDos[2].taskName}
-                taskAssignee={toDos[2].taskAssignee}
-              />
-              <ToDoRowItem
-                rowNumber={toDos[3].rowNumber}
-                taskName={toDos[3].taskName}
-                taskAssignee={toDos[3].taskAssignee}
-              />
-            </tbody>
-          </table>
+          <TodoTable toDos={toDos} />
         </div>
       </div>
     </div>
---
+++ src/components/TodoTable.js
@@ -0,0 +1,28 @@
+import ToDoRowItem from './ToDoRowItem';
+
+function TodoTable(props) {
+  return (
+    <table className='table table-hover'>
+      <thead>
+        <tr>
+          <th scope='col'>#</th>
+          <th scope='col'>Description</th>
+          <th scope='col'>Assignee</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.toDos.map((todo) => {
+          return (
+            <ToDoRowItem
+              rowNumber={todo.rowNumber}
+              taskName={todo.taskName}
+              taskAssignee={todo.taskAssignee}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default TodoTable;
