f0afcaea587df2c669d3f0068b2345013e83c200
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-04-21 20:09:12 +0600

    added a button and event handler below the toDos table.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import TodoTable from './components/TodoTable';
+import React, { useState } from 'react';
+
 const toDos = [
   { rowNumber: 1, taskName: 'Learn React', taskAssignee: 'Arka' },
   { rowNumber: 2, taskName: 'Revise Python', taskAssignee: 'Arka' },
@@ -7,12 +9,28 @@ const toDos = [
   { rowNumber: 4, taskName: 'Learn Django', taskAssignee: 'Arka' }
 ];
 function App() {
+  const addTodo = () => {
+    if (toDos.length > 0) {
+      const newToDo = {
+        rowNumber: toDos.length + 1,
+        taskName: 'New Task',
+        taskAssignee: 'Arka'
+      };
+
+      toDos.push(newToDo);
+      console.log(toDos);
+    }
+  };
+
   return (
     <div className='mt-5 container'>
       <div className='card'>
         <div className='card-header'>Your Todo's List</div>
         <div className='card-body'>
           <TodoTable toDos={toDos} />
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new To-Do Item
+          </button>
         </div>
       </div>
     </div>
